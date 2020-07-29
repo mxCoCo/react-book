926eeeafff054eeee8c64364f0b75dc1fc6dc337
mxCoCo <862617618@qq.com>
2020-07-29 20:58:29 +0800

    完成

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import {
   DetailWrapper,
+  Header,
+  Content,
 } from './styled'
 
 export class Detail extends Component {
   render () {
     return (
       <DetailWrapper>
-        hello Detail
+        <Header>为什么被你们吹爆了的《哪吒》，我只看到了满屏的尴尬？</Header>
+        <Content>
+          <img className="pic" src="https://upload-images.jianshu.io/upload_images/12081621-b05fe51264d22dcb.jpg?imageMogr2/auto-orient/strip|imageView2/2/w/400/format/webp" alt=""></img>
+          <p>
+            <b>昨天我看了个动画片。</b>片子讲的是一个村里有个小孩子自出生起体内就封印了一股强大的妖力，于是村民们把他当做妖怪，畏惧他，疏远他，避之唯恐不及。他表面上满不在乎，在村子里为非作歹，但内心深处却感到巨大的孤独，极度渴望被世人接纳……
+            我说的不是《火影忍者》，是《哪吒》。
+          </p>
+        </Content>
       </DetailWrapper>
     )
   }
--- src/pages/detail/styled.js
+++ src/pages/detail/styled.js
@@ -1,7 +1,33 @@
 import styled from 'styled-components'
 
 export const DetailWrapper = styled.div`
-  width: 960px;
+  width: 620px;
   margin: 0 auto;
+  padding-bottom: 100px;
   overflow: hidden;
+  
+`
+export const Header = styled.div`
+  width: 620px;
+  margin: 50px 0 20px 0;
+  line-height: 44px;
+  font-size: 34px;
+  color: #333;
+  font-weight: bold;
+`
+export const Content = styled.div`
+  font-size: 15px;
+  color: #2f2f2f;
+  .pic {
+    width: 100%;
+    height: 620px;
+  }
+  p {
+    margin: 25px 0;
+    font-size: 16px;
+    line-height: 30px;
+  }
+  b {
+    font-weight: bold;
+  }
 `
